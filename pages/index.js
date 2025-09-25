54f90cccc1724b4edfe7577de05fe4ae5f3c3134
Automatech <thalissonsousa17@gmail.com>
2025-09-25 17:38:39 -0300

    Desafio de aula

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      Amor, na próxima semana não estarei apenas casando com você, estarei
+      escolhendo, todos os dias da minha vida, caminhar ao seu lado, ser seu
+      porto seguro e o maior fã dos seus sonhos.
+    </h1>
+  );
+}
+
+export default Home;
